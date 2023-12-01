40b620d90810b10200eb059a60d9fdd56f80e635
sejum-x <franchukivan123@gmail.com>
2023-12-01 22:33:00 +0200

    model

---
+++ MWash/Model/MWashAccounting.cs
@@ -0,0 +1,38 @@
+﻿using MWash.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MWash
+{
+    public class MWashAccounting
+    {
+        // Колекція для зберігання записів про надані послуги
+        public ObservableCollection<ServiceRecord> ServiceRecords { get; private set; }
+
+        public MWashAccounting()
+        {
+            ServiceRecords = new ObservableCollection<ServiceRecord>();
+        }
+
+        // Метод для додавання нового запису про надану послугу
+        public void AddServiceRecord(ServiceRecord serviceRecord)
+        {
+            ServiceRecords.Add(serviceRecord);
+        }
+
+        // Інші методи для реалізації функціональних вимог
+
+        // Наприклад, методи для керування працівниками, додавання та видалення послуг тощо
+
+        // Наприклад:
+        // public void AddEmployee(Employee employee) { ... }
+        // public void RemoveEmployee(Employee employee) { ... }
+        // public void AddService(Service service) { ... }
+        // public void GenerateDailyReport(DateTime date) { ... }
+        // і т.д.
+    }
+}
---
+++ MWash/Model/Service.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MWash
+{
+    public class Service
+    {
+        public string ServiceName { get; set; }
+        public int ServiceCost { get; set; }
+
+        // Конструктор класу Service
+        public Service(string serviceName, int serviceCost)
+        {
+            ServiceName = serviceName;
+            ServiceCost = serviceCost;
+        }
+    }
+}
