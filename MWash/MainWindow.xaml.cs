baa665ad822b5a62cc35ffe1c85b34c66fe52a6b
sejum-x <franchukivan123@gmail.com>
2023-12-01 23:52:58 +0200

    main table output

--- MWash/MainWindow.xaml.cs
+++ MWash/MainWindow.xaml.cs
@@ -1,37 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MWash
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private MWashAccounting accounting; // Додайте це поле
+
         public MainWindow()
         {
             InitializeComponent();
-        }
+            accounting = new MWashAccounting(); // Ініціалізуйте об'єкт MWashAccounting
+            PopulateServiceDataGrid();
 
-        private void exitSalaryButton_Click(object sender, RoutedEventArgs e)
-        {
-            DoubleAnimation fadeOutAnimation = new DoubleAnimation(0, TimeSpan.FromSeconds(0.2));
-            Salary.BeginAnimation(UIElement.OpacityProperty, fadeOutAnimation);
-
-            Salary.IsHitTestVisible = false;
+            // ... інші ініціалізації або налаштування інтерфейсу
         }
 
         private void openSalaryButton_Click(object sender, RoutedEventArgs e)
@@ -40,6 +25,44 @@ namespace MWash
             Salary.BeginAnimation(UIElement.OpacityProperty, fadeOutAnimation);
 
             Salary.IsHitTestVisible = true;
+                        
+        }
+
+        private void PopulateServiceDataGrid()
+        {
+            // Захардкоджена інформація для відображення в таблиці
+            var hardcodedData = new List<(string Employee, string Service, int Price, DateTime Time)>
+        {
+        ("John Doe", "Car Wash", 50, new DateTime(2023, 12, 1, 10, 30, 0)),
+        ("Jane Smith", "Interior Cleaning", 80, new DateTime(2023, 12, 1, 11, 0, 0)),
+        ("Alice Johnson", "Wheel Maintenance", 30, new DateTime(2023, 12, 1, 12, 15, 0)),
+        // Додайте інші дані за необхідністю
+        };
+
+            // Створення нового списку об'єктів для відображення в DataGrid
+            var serviceDataList = hardcodedData.Select((data, index) => new
+            {
+                Number = index + 1,
+                data.Employee,
+                data.Service,
+                data.Price,
+                Time = data.Time.ToString("dd MMMM, yyyy")
+            }).ToList();
+
+            // Очистити ServiceDataGrid перед додаванням нових даних
+            ServiceDataGrid.ItemsSource = null;
+
+            // Додавання нових даних до ServiceDataGrid
+            ServiceDataGrid.ItemsSource = serviceDataList;
+        }
+
+
+        private void exitSalaryButton_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation fadeOutAnimation = new DoubleAnimation(0, TimeSpan.FromSeconds(0.2));
+            Salary.BeginAnimation(UIElement.OpacityProperty, fadeOutAnimation);
+
+            Salary.IsHitTestVisible = false;
         }
     }
 }
